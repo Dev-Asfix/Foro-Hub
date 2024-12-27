20a104730e24182373a559e52a2fe6ab65ee8ab1
Dev Asfix <rosasramirez753@gmail.com>
2024-12-27 12:36:07 -0500

    dto user

--- src/main/java/com/alura/foro/foro_hub/controller/UsuarioController.java
+++ src/main/java/com/alura/foro/foro_hub/controller/UsuarioController.java
@@ -1,4 +1,14 @@
 package com.alura.foro.foro_hub.controller;
 
+import com.alura.foro.foro_hub.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 }
---
+++ src/main/java/com/alura/foro/foro_hub/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package com.alura.foro.foro_hub.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
