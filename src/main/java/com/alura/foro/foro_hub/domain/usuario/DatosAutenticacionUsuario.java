5bb73c663dc0fec91e40166c0e69246b9ff94bee
Dev Asfix <rosasramirez753@gmail.com>
2025-01-02 19:09:57 -0500

    generar un token

---
+++ src/main/java/com/alura/foro/foro_hub/domain/usuario/DatosAutenticacionUsuario.java
@@ -0,0 +1,4 @@
+package com.alura.foro.foro_hub.domain.usuario;
+
+public record DatosAutenticacionUsuario(String correo_electronico , String contrasena) {
+}
--- src/main/java/com/alura/foro/foro_hub/domain/usuario/Usuario.java
+++ src/main/java/com/alura/foro/foro_hub/domain/usuario/Usuario.java
@@ -3,6 +3,12 @@ package com.alura.foro.foro_hub.domain.usuario;
 import com.alura.foro.foro_hub.domain.perfil.Perfil;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "usuario")
 @Table(name = "usuarios")
@@ -11,7 +17,7 @@ import lombok.*;
 //@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of= "id")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -75,5 +81,40 @@ public class Usuario {
 
     public void setPerfil(Perfil perfil) {
         this.perfil = perfil;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return contrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return correo_electronico;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
